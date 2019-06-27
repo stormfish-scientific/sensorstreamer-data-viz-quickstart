e8648d1eec0cc75e754a8dcce75264a35b4ab320
Theron T. Trout <theron.trout@stormfish.io>
2019-06-27 17:12:29 -0400

    Added real-time streaming query example code

--- src/estest2.py
+++ src/estest2.py
@@ -2,6 +2,8 @@ from datetime import datetime
 from elasticsearch import Elasticsearch
 from pprint import pprint
 import math
+import time
+import signal
 
 import os
 
@@ -10,79 +12,77 @@ import argparse
 
 es = Elasticsearch(os.environ["DOCKER_HOST"] + ":9200")
 
-res = es.search(
-    index="sensor-stream-*",
-    body="""
+query_template = """
 {
-  "version": true,
-  "size": 500,
-  "sort": [
-    {
-      "@timestamp": {
-        "order": "desc",
-        "unmapped_type": "boolean"
-      }
-    }
-  ],
-  "_source": {
-    "excludes": []
-  },
-  "aggs": {
-    "2": {
-      "date_histogram": {
-        "field": "@timestamp",
-        "interval": "30s",
-        "time_zone": "America/New_York",
-        "min_doc_count": 1
-      }
-    }
-  },
-  "stored_fields": [
-    "*"
-  ],
-  "script_fields": {},
-  "docvalue_fields": [
-    {
-      "field": "@timestamp",
-      "format": "date_time"
-    }
-  ],
-  "query": {
-    "bool": {
-      "must": [
+    "version": true,
+    "size": 500,
+    "sort": [
         {
-          "range": {
-            "@timestamp": {
-              "format": "strict_date_optional_time",
-              "gte": "2019-06-25T18:23:57.641Z",
-              "lte": "2019-06-25T18:38:57.641Z"
-            }
-          }
+            "@timestamp": "asc"
         }
-      ],
-      "filter": [
+    ],
+    "docvalue_fields": [
         {
-          "match_all": {}
+            "field": "@timestamp",
+            "format": "date_time"
         }
-      ],
-      "should": [],
-      "must_not": []
+    ],
+    "query": {
+        ${QUERY}
     }
-  },
-  "highlight": {
-    "pre_tags": [
-      "@kibana-highlighted-field@"
-    ],
-    "post_tags": [
-      "@/kibana-highlighted-field@"
-    ],
-    "fields": {
-      "*": {}
-    },
-    "fragment_size": 2147483647
-  }
+    ${EXTRA}
 }
 """
+
+basic_query = """
+"bool": {
+    "must": [
+        {
+            "range": {
+                "@timestamp": {
+                    "format": "strict_date_optional_time",
+                    "gte": "now-1m",
+                    "lte": "now"
+                }
+            }
+        }
+    ],
+    "filter": [
+        {
+            "match_all": {}
+        }
+    ],
+    "should": [],
+    "must_not": []
+}
+"""
+
+
+def prepare_query(query=None, extra=None):
+    global query_template
+    global basic_query
+
+    if query is None:
+        query = basic_query
+
+    if extra is None:
+        extra = ''
+
+    if extra != '':
+        extra = ",\n" + extra
+
+    res = query_template.replace("${QUERY}", query)
+
+    res = res.replace("${EXTRA}", extra)
+
+    return res
+
+
+q = prepare_query()
+
+res = es.search(
+    index="sensor-stream-*",
+    body=q
 )
 
 
@@ -101,11 +101,66 @@ def normalize(vec, mag=None):
     return [vec[0]/mag, vec[1]/mag, vec[2]/mag]
 
 
-print("Got %d Hits:" % res['hits']['total']['value'])
-for hit in res['hits']['hits']:
-    vec = hit["_source"]['gravity']['value']
-    mag = magnitude(vec)
-    norm = normalize(vec, mag)
-    pprint({'gravity': vec,
-            'magnitude': mag,
-            'norm': norm})
+keep_running = True
+
+search_after_val = None
+
+while keep_running:
+
+    try:
+
+        found = False
+
+        # print("Got %d Hits:" % res['hits']['total']['value'])
+
+        # pprint(len(res['hits']['hits']))
+
+        #pprint(res['hits']['hits'])
+        for hit in res['hits']['hits']:
+
+            search_after_val = hit['sort'][0]
+            found = True
+
+            if 'value' in hit['_source']['gravity']:
+                vec = hit["_source"]['gravity']['value']
+                mag = magnitude(vec)
+                norm = normalize(vec, mag)
+                #print(hit['_source']['@timestamp'] + str(vec))
+
+                timestamp = search_after_val / 1000.0
+                print("%0.3f - %s" % (timestamp, str(vec)))
+
+                # pprint({'gravity': vec,
+                #         'magnitude': mag,
+                #         'norm': norm})
+
+        if found:
+            print('-')
+                
+
+        #pprint({'search_after_val': search_after_val})
+
+        if search_after_val is not None:
+            q = prepare_query(
+                None,
+                '"search_after": [' + str(search_after_val) + "]"
+            )
+
+            # print("---")
+            # print(q)
+            # print("---")
+
+        res = es.search(
+            index="sensor-stream-*",
+            body=q
+        )
+
+        # Do a little sleep
+        time.sleep(.25)
+
+    except KeyboardInterrupt:
+        keep_running = False
+
+    except Exception as ex:
+        keep_running = False
+        raise ex
